becfc4dc2fa9fccd00d6a088a36d7a2309004f76
Ron Reynolds <tequilaron@gmail.com>
2012-05-29 17:34:03 -0700

    added comment to assertEquals(String,DateTime,DateTime)

--- test/ovation/odata/util/JUnitUtils.java
+++ test/ovation/odata/util/JUnitUtils.java
@@ -32,6 +32,18 @@ public class JUnitUtils {
     public static void assertEquals(DateTime expected, DateTime actual) {
         assertEquals(null, expected, actual);
     }
+    /**
+     * had to create our own cuz this makes no sense:
+     * 
+     * junit.framework.AssertionFailedError: ovation:///93298e5d-4819-4a55-8ebf-9b923e82d86a/#2-1001-1-5:1000048 
+     * expected:<2012-04-06T02:18:17.109-05:00>
+     *  but was:<2012-04-06T02:18:17.109-05:00>
+     * unless DateTime.equals() isn't comparing anything or it's comparing the wrong things. :-/
+     * 
+     * @param msg
+     * @param expected
+     * @param actual
+     */
     public static void assertEquals(String msg, DateTime expected, DateTime actual) {
         if (expected == actual) return; // handles identical arrays and both null
         if (expected == null || actual == null) Assert.assertEquals(msg, expected, actual); // guaranteed to fail
